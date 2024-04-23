3ae3b6abae7944ad3b24a0cb51b91a2d63cb59dc
Dk1080 <102157310+Dk1080@users.noreply.github.com>
2024-04-23 13:04:20 +0200

    First Fixes

--- Tests/InitialVisitorTest.java
+++ Tests/InitialVisitorTest.java
@@ -1,8 +1,12 @@
 import ASTNode.ProgramNode;
+import AntlrGenFiles.*;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InitialVisitorTest {
 
@@ -21,10 +25,29 @@ public class InitialVisitorTest {
     @Test
     void VisitProgramNodeTest() {
 
+        // Can the function be insterted into the function map?
         var AST = setupAST("begin function hello(); var x = 2; end function;");
         System.out.println(AST);
-        var InitialVisitor = new InitialVisitor();
-        System.out.println(InitialVisitor.Visit(AST));
+        NybCStack nybCStack = new NybCStack();
+        List<String> keywords = new ArrayList<>();
+        var InitialVisitor = new InitialVisitor(nybCStack,keywords);
+        InitialVisitor.Visit(AST);
+        System.out.println(nybCStack.getFmap().toString());
+
+        // Can a function with the same name happen twice?
+        AST = setupAST("begin function hello(); var x = 2; end function; begin function hello(); var x = 2; end function;");
+        System.out.println(AST);
+        nybCStack = new NybCStack();
+        keywords = new ArrayList<>();
+        InitialVisitor = new InitialVisitor(nybCStack,keywords);
+        InitialVisitor.Visit(AST);
+        System.out.println(nybCStack.getFmap().toString());
+
+
+
+        // Can a function with the same name as a keyword happen?
+
+
 
     }
 
